e0bb00a1f83394f2825f9fd4594c9f2cbba35c10
Kbowers28 <kb7674@gmail.com>
2025-06-13 18:51:05 -0500

    Update bot_sync_verified.py

--- bot_sync_verified.py
+++ bot_sync_verified.py
@@ -99,7 +99,7 @@ def webhook():
         qty = calculate_qty(entry, stop, RISK_PERCENT, ACCOUNT_SIZE)
         tp = entry + (entry - stop) * 2 if side == "BUY" else entry - (stop - entry) * 2
 
-        ib.connect("209.145.57.242", 4002, clientId=22)
+        ib.connect("127.0.0.1", 4002, clientId=22)
         if not ib.isConnected():
             return jsonify({"error": "IBKR not connected"}), 500
 
